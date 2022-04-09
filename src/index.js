2b999b0abbc557d35bf77ed55fd5ac7942de0ed6
Kaiyang Yao <kyao@macalester.edu>
2022-04-09 15:11:11 -0500

    update

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import { render } from "react-dom";
 import Dashboard from "./components/Dashboard";
 import Main from "./components/Main";
+import Calendar from "./components/Calendar";
 
 render(<Main />, document.getElementById("root"));
