0f455b69afa5fe96e6ab489044f2daaefc54a70b
Kaiyang Yao <kyao@macalester.edu>
2022-04-21 22:57:44 -0500

    debug location

--- src/components/Events/SingleEvent.js
+++ src/components/Events/SingleEvent.js
@@ -1,41 +1,56 @@
 import { Card, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Stack} from "@mui/material";
-import {useDispatch} from 'react-redux';
-import {changeBuildings} from "../../features/buildingsSlice";
-
+import { Stack } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { changeBuildings } from "../../features/buildingsSlice";
 
 function SingleEvent(props) {
   var event = props.event;
   const dispatch = useDispatch();
-  var buildings =list["olin rice", "humanity", "janet wallace fine art center", "leonard center",
-"old main", "carnegie", "library", "campus center", "weyerhauser hall", "mac stadium", 
-"weyerhauser chapel", "kagin"]
-  const handleOnClick = () =>{
-    var location = event.location
-    var index = 0
-    for (let i = 0; i < buildings.length; i++){
-      if (buildings[i].length > location.length){
-        if (buildings[i].toLowercase().includes(location)){
-          index = i
+  var buildings = [
+    "olin rice",
+    "humanity",
+    "janet wallace fine art center",
+    "leonard center",
+    "old main",
+    "carnegie",
+    "library",
+    "campus center",
+    "weyerhauser hall",
+    "mac stadium",
+    "weyerhauser chapel",
+    "kagin",
+  ];
+  const handleOnClick = () => {
+    var location = event.location.toLowerCase();
+    var index = 0;
+    for (let i = 0; i < buildings.length; i++) {
+      if (buildings[i].length > location.length) {
+        if (buildings[i].includes(location.toLowerCase())) {
+          index = i;
         }
-      }
-      else{
-        if (location.includes(buildings[i].toLowercase())){
-          index = i
+      } else {
+        console.log("bu", buildings[i]);
+        console.log("loc", location);
+        if (location.includes(buildings[i])) {
+          console.log("!!!");
+          console.log(location);
+          
+          console.log(buildings[i]);
+          index = i;
         }
       }
     }
-    if (location.includes("weyerhauser")){
-      if (location.includes("hall")){
-        index = 8
+    if (location.includes("weyerhaeuser")) {
+      if (location.includes("hall")) {
+        index = 8;
       }
-      if (location.includes("chapel")){
-        index = 10
+      if (location.includes("chapel")) {
+        index = 10;
       }
     }
     dispatch(changeBuildings(index));
-  }
+  };
 
   return (
     <div>
@@ -48,11 +63,13 @@ function SingleEvent(props) {
           </Card.Subtitle>
           <Card.Text>{event.location}</Card.Text>
 
-          <Stack direction="row" spacing={2}   justifyContent="space-between">
-
-          <a href= {event.url} class="card-link" target = "_blank">Card link</a>
-          <Button variant="primary" size = 'sm' onClick = {handleOnClick }>Show On Map </Button>
-
+          <Stack direction="row" spacing={2} justifyContent="space-between">
+            <a href={event.url} class="card-link" target="_blank">
+              Card link
+            </a>
+            <Button variant="primary" size="sm" onClick={handleOnClick}>
+              Show On Map{" "}
+            </Button>
           </Stack>
         </Card.Body>
       </Card>
