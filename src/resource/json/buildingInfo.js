0047b547cab7bd4d2256c4f80689433e06b5b1b2
scottyehengzong <yzong@macalester.edu>
2022-04-30 21:28:20 -0500

    coordinate
    info that helps the map to center at one building when the building is clicked

--- src/resource/json/buildingInfo.js
+++ src/resource/json/buildingInfo.js
@@ -4,84 +4,96 @@ var buildingInfo = [
         "name": "olin rice",
         "descrip": "STEM classrooms and labs to support students and faculty research.",
         "in this building": "Biology; Chemistry; Environmental Studies; Geology; Mathematics, Statistics, and Computer Science; Neuroscience; Physics and Astronomy; Psychology; Henry Lepp Geology Museum; Smail Gallery; The Olin-Rice Hub",
-        "website": None
+        "website": None,
+        "coordinate": [44.93674934793792, -93.16896676039332]
     },
     {
         "index": "1",
         "name": "humanity",
         "descrip": "Most language programs find their home here, along with the Digital Resource Center and Information Technology Services.",
         "in this building": "American Studies; Asian Languages and Cultures; Asian Studies; Educational Studies; French Studies; German Studies; Linguistics; Media and Cultural Studies; Russian Studies; Spanish and Portuguese.",
-        "website": None
+        "website": None,
+        "coordinate": [44.93727887238539, -93.16929919486192]
     },
     {
         "index": "2",
         "name": "janet wallace fine art center",
         "descrip": "J-Wall is a collection of buildings with space for music, art, dance, and theater. Lots of lounge and study spaces are in this building. In 2019, the Theater and Dance Building was the latest section to be renovated.",
         "in this building": "Law Warschaw Gallery; Lowe Dayton Commons; Ressiner Lounge; Joan Adams Mondale Hall of Studio Art; Mairs Concert Hall",
-        "website": "https://www.macalester.edu/arts-at-mac/"
+        "website": "https://www.macalester.edu/arts-at-mac/",
+        "coordinate": [44.93767004198347, -93.1696507714153]
     },
     {
         "index": "3",
         "name": "leonard center",
         "descrip": "The Leonard Center Athletic Complex provides facilities for athletics, fitness, recreation, community gatherings and large campus events. The Laurie Hamre Center for Health and Wellness is located here for students to access medical care and other services.",
         "in this building": "Alumni Gymnasium; Deno Fitness; Fieldhouse; Hultquist Lounge; Nystrom Hall of Fame Room; Riley Pool; Schall Family Atrium; Scotty’s; Wellness Coach",
-        "website": "https://athletics.macalester.edu/sports/2008/7/10/GEN_0710084041.aspx"
+        "website": "https://athletics.macalester.edu/sports/2008/7/10/GEN_0710084041.aspx",
+        "coordinate": [44.93768143367376, -93.16731173492323]
     },
     {
         "index": "4",
         "name": "old main",
         "descrip": "This campus icon was built in 1887 and houses several humanities departments and classrooms. ",
         "in this building": "Classical Mediterranean and Middle East; English; History; Philosophy; Religious Studies; Women’s Gender and Sexuality Studies",
-        "website": None
+        "website": None,
+        "coordinate": [44.938566592611515, -93.1686734859453]
     },
     {
         "index": "5",
         "name": "carnegie",
         "descrip": "Home to classrooms and labs and a rotating selection of flags representing the home countries of current.",
         "in this building": "Anthropology; Economics; Geography; International Studies; Political Science; Sociology.",
-        "website": None
+        "website": None,
+        "coordinate": [44.938724158737436, -93.1692234238339]
     },
     {
         "index": "6",
         "name": "library",
         "descrip": "Ample study space with access to a large collection of books, periodicals, and a computer lab. Library staff can help with interlibrary loans, research, and more, The Ideal Lab is a great space to connect with other students and lean into creativity.",
         "in this building": "Library; Entrepreneurship and Innovation; Ideal Lab; Jan Serie Center for Scholarship and Teaching; Harmon Room ",
-        "website": "https://www.macalester.edu/library/"
+        "website": "https://www.macalester.edu/library/",
+        "coordinate": [44.93849707800676, -93.16818246997333]
     },
     {
         "index": "7",
         "name": "campus center",
         "descrip": "The hub of student life. Meals are served in Cafe Mac on the first floor, with meeting and lounge space on the second. The lower floor includes student mail boxes and more group spaces. ",
         "in this building": "Cafe Mac; Campus Operations; John B. Davis Lecture Hall; The Loch Student Lounge; Mailing and Document Services; McPherson Atrium; Office of Student Leadership $ Engagement; Residential Life",
-        "website": "https://www.macalester.edu/campus-center/home/campuscenter/"
+        "website": "https://www.macalester.edu/campus-center/home/campuscenter/",
+        "coordinate": [44.93968576618459, -93.16740666468414]
     },
     {
         "index": "8",
         "name": "weyerhauser hall",
         "descrip": "The Admissions Office is located on the lower level of Weyerhaeuser Hall. Academic Programs and Advising and the Office of Administration and Finance are located on the second floor of Weyerhaeuser Hall.",
         "in this building": "Admissions Office, Academic Programs and Advising; Administration and Finance",
-        "website": None
+        "website": None,
+        "coordinate": [44.93947027483801, -93.16922342385783]
     },
     {
         "index": "9",
         "name": "mac stadium",
         "descrip": "NCAA Div III teams compete on the fields located in the southern tier of campus. Fields include Macalester Stadium; Dayton track; Nicholson Field; Softball Field and Hayden Tennis Courts.",
         "in this building": "Macalester Stadium; Dayton track; Nicholson Field; Softball Field; Hayden Tennis Courts.",
-        "website": "https://athletics.macalester.edu/sports/2008/11/11/GEN_1111081258.aspx"
+        "website": "https://athletics.macalester.edu/sports/2008/11/11/GEN_1111081258.aspx",
+        "coordinate": [44.93530511114528, -93.16807803262917]
     },
     {
         "index": "10",
         "name": "weyerhauser chapel",
         "descrip": "The ground floor is the Weyerhaeuser Memorial Chapel. The chapel is a gathering place for the Macalester community, providing space for reflection, rest, connection, and spiritual practice. The lower level of our building is the Center for Religious and Spiritual Life.",
         "in this building": "Weyerhaeuser Memorial Chapel; Center for Religious and Spiritual Life",
-        "website": "https://www.macalester.edu/religiouslife/about-us/"
+        "website": "https://www.macalester.edu/religiouslife/about-us/",
+        "coordinate": [44.93973210830617, -93.16868003284885]
     },
     {
         "index": "11",
         "name": "kagin",
         "descrip": "Kagin Commons’ upper level is home to the Alexander G. Hill Ballroom, used for dances, concerts, room draw,  workshops, speakers, and other events.",
         "in this building": "Career Exploration; Disability Services; International Student Programs; Internship Program; Lealtad-Suzuki Center; MAX (Macalester Academic Excellence) Center; Multicultural Life.",
-        "website": None
+        "website": None,
+        "coordinate": [44.940696015938656, -93.16749832099467]
     }
     
 ];
